5ab9c98bdba502e1198bc2c2cfcd91a888f9ea4a
initmiss <119107563+initmiss@users.noreply.github.com>
2020-11-18 18:53:30 +0000

    write some css

--- IndiegalaFreebieNotifier/Model/Record/FreeGameRecord.cs
+++ IndiegalaFreebieNotifier/Model/Record/FreeGameRecord.cs
@@ -7,4 +7,4 @@ namespace IndiegalaFreebieNotifier.Model {
 		public string Title { get; set; }
 
 		public string ToTelegramMessage() {
-			return new StringBuilder(
+			return new StringBuilder().AppendFormat(Notif
