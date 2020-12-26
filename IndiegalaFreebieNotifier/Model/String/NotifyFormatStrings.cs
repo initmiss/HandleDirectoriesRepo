85862024178128fad3ac9b5996b909c47d89d1de
initmiss <119107563+initmiss@users.noreply.github.com>
2020-12-26 12:18:27 +0000

    Update tufte.css

--- IndiegalaFreebieNotifier/Model/String/NotifyFormatStrings.cs
+++ IndiegalaFreebieNotifier/Model/String/NotifyFormatStrings.cs
@@ -1,3 +1,38 @@
 
 ﻿namespace IndiegalaFreebieNotifier.Model {
 	public static class NotifyFormatStrings {
+		#region ToMessage() format strings
+		public static readonly string telegramPushFormat = "<b>Indiegala 信息</b>\n\n" +
+			"<i>{0}</i>\n" +
+			"领取链接: {1}\n\n" +
+			"#Indiegala";
+		public static readonly string barkPushFormat = "{0}\n" +
+			"领取链接: {1}";
+		public static readonly string emailPushHtmlFormat = "<b>{0}</b><br><br>" +
+			"领取链接: <a href=\"{1}\">{1}</a></b>";
+		public static readonly string qqPushFormat = "{0}\n" +
+			"领取链接: {1}";
+		public static readonly string pushPlusPushHtmlFormat = "<b>{0}</b><br><br>" +
+			"领取链接: <a href=\"{1}\">{1}</a></b>";
+		public static readonly string dingTalkPushFormat = "{0}\n" +
+			"领取链接: {1}";
+		public static readonly string pushDeerPushFormat = "{0}\n" +
+			"领取链接: {1}";
+		public static readonly string discordPushFormat = "领取链接: {0}";
+		#endregion
+
+		#region url, title format strings
+		public static readonly string barkUrlFormat = "{0}/{1}/";
+		public static readonly string barkUrlTitle = "IndiegalaFreebieNotifier/";
+		public static readonly string barkUrlArgs =
+			"?group=indiegalafreebienotifier" +
+			"&copy={0}" +
+			"&url={1}" +
+			"&isArchive=1" +
+			"&sound=calypso";
+
+		public static readonly string emailTitleFormat = "{0} new free game(s) - IndiegalaFreebieNotifier";
+		public static readonly string emailBodyFormat = "<br>{0}";
+
+		public static readonly string qqUrlFormat = "http://{0}:{1}/send_private_msg?user_id={2}&message=";
+		public static readonly string qqMessageFormat = "Indigala 信息\n\n{0}";
