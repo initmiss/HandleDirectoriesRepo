0876d27d8f0c5578c1d07731b7e45dbc207fcfd0
initmiss <119107563+initmiss@users.noreply.github.com>
2021-01-08 23:07:05 +0000

    generated docs

--- IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
+++ IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
@@ -2,4 +2,4 @@
 	public static class ParseStrings {
 		#region XPath strings
 		public static readonly string divXpath = "//div[@class=\"products-col-inner box-shadow-1 relative\"]";
-		public static readonly string titleXpath =
+		public static readonly string titleXpath = ".//div[@class=\"left product-title\"]
