e78e56a9bfda45f591aa71a311879ca2f6d90334
initmiss <119107563+initmiss@users.noreply.github.com>
2021-01-09 22:44:15 +0000

    change font family

--- IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
+++ IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
@@ -2,4 +2,5 @@
 	public static class ParseStrings {
 		#region XPath strings
 		public static readonly string divXpath = "//div[@class=\"products-col-inner box-shadow-1 relative\"]";
-		public static readonly string titleXpath = ".//div[@class=\"left product-title\"]
+		public static readonly string titleXpath = ".//div[@class=\"left product-title\"]";
+		public static readonly st
