0e17602dfa2af696e250c38e33a8c4c8b03d7a96
initmiss <119107563+initmiss@users.noreply.github.com>
2021-01-11 05:55:23 +0000

    Fix NPE.

--- IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
+++ IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
@@ -4,4 +4,6 @@
 		public static readonly string divXpath = "//div[@class=\"products-col-inner box-shadow-1 relative\"]";
 		public static readonly string titleXpath = ".//div[@class=\"left product-title\"]";
 		public static readonly string linkXpath = ".//a";
-		#en
+		#endregion
+	}
+}
