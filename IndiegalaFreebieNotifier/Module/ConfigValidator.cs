85f892b859ef611507055a1c8c954fb37d36fc3f
initmiss <119107563+initmiss@users.noreply.github.com>
2021-02-21 14:43:23 +0000

    Add .gitignore and .gitattributes.

--- IndiegalaFreebieNotifier/Module/ConfigValidator.cs
+++ IndiegalaFreebieNotifier/Module/ConfigValidator.cs
@@ -23,4 +23,4 @@ namespace IndiegalaFreebieNotifier.Module {
 					if (string.IsNullOrEmpty(config.TelegramToken))
 						throw new Exception(message: "No Telegram Token provided!");
 					if (string.IsNullOrEmpty(config.TelegramChatID))
-						t
+						throw new Exception(message: "No 
