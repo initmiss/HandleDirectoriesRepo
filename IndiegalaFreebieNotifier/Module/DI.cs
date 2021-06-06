24c77c51f0b2e2dfc24448f6c5643d1a98087164
initmiss <119107563+initmiss@users.noreply.github.com>
2021-06-06 16:27:39 +0000

    Fix namespaces

--- IndiegalaFreebieNotifier/Module/DI.cs
+++ IndiegalaFreebieNotifier/Module/DI.cs
@@ -2,4 +2,5 @@
 using System.IO;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Depend
+using Microsoft.Extensions.DependencyInjection;
+using NL
