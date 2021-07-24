3e584509b5c6b9e1dd344f91573362ea0e514ecd
initmiss <119107563+initmiss@users.noreply.github.com>
2021-07-24 20:33:08 +0000

    remove extra comment

--- IndiegalaFreebieNotifier/Module/DI.cs
+++ IndiegalaFreebieNotifier/Module/DI.cs
@@ -28,4 +28,4 @@ namespace IndiegalaFreebieNotifier.Module {
                .AddTransient<Discord>()
                .AddLogging(loggingBuilder => {
                    // configure Logging with NLog
- 
+                   loggingBuilder
