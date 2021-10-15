8a523c45350659d7d14e2ea8d08813c6e16b4ab1
initmiss <119107563+initmiss@users.noreply.github.com>
2021-10-15 17:35:45 +0000

    Bumping version to 0.5.0

--- IndiegalaFreebieNotifier/Module/JsonOP.cs
+++ IndiegalaFreebieNotifier/Module/JsonOP.cs
@@ -9,4 +9,6 @@ namespace IndiegalaFreebieNotifier.Module {
 	class JsonOP : IDisposable {
 		private readonly ILogger<JsonOP> _logger;
 		private readonly string configPath = $"{AppDomain.CurrentDomain.BaseDirectory}Config{Path.DirectorySeparatorChar}config.json";
-		private readonly string recordPath = $"{AppDomain.CurrentDomain.BaseDirectory}Record{Path.DirectorySeparatorChar}record
+		private readonly string recordPath = $"{AppDomain.CurrentDomain.BaseDirectory}Record{Path.DirectorySeparatorChar}record.json";
+
+		public JsonOP(ILogger<J
