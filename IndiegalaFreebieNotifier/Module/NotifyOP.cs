a6d3aa595945189e6e781bfe5023f6a19e729b5f
initmiss <119107563+initmiss@users.noreply.github.com>
2022-01-15 14:04:51 +0000

    Version 2.8

--- IndiegalaFreebieNotifier/Module/NotifyOP.cs
+++ IndiegalaFreebieNotifier/Module/NotifyOP.cs
@@ -45,4 +45,5 @@ namespace IndiegalaFreebieNotifier.Module {
 
 				//QQ notifications
 				if (config.EnableQQ) {
-					_logger.LogInformation(debu
+					_logger.LogInformation(debugEnabledFormat, "QQ");
+					awai
