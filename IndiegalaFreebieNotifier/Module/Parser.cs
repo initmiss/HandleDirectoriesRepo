f501457128f6217f526644da9bac0e4e00ae9901
initmiss <119107563+initmiss@users.noreply.github.com>
2022-04-16 17:09:46 +0000

    oom_kill: has_intersects_mems_allowed() needs rcu_read_lock()

--- IndiegalaFreebieNotifier/Module/Parser.cs
+++ IndiegalaFreebieNotifier/Module/Parser.cs
@@ -29,4 +29,4 @@ namespace IndiegalaFreebieNotifier.Module {
 						var title = each.SelectSingleNode(ParseStrings.titleXpath).InnerText;
 						var link = each.SelectSingleNode(ParseStrings.linkXpath).Attributes["href"].Value;
 
-	
+						_logger.LogInformation("Found ne
