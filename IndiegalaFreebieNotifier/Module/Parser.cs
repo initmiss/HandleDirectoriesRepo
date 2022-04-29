be0a610668fb125510619760a69886ee547ed631
initmiss <119107563+initmiss@users.noreply.github.com>
2022-04-29 21:20:35 +0000

    pr fixes

--- IndiegalaFreebieNotifier/Module/Parser.cs
+++ IndiegalaFreebieNotifier/Module/Parser.cs
@@ -41,4 +41,6 @@ namespace IndiegalaFreebieNotifier.Module {
 							parseResult.PushList.Add(newFreeGame);
 						} else _logger.LogInformation("{0} is found in previous records, stop adding it to push list", title);
 					}
-				} else _logger.LogInformation("No freebies cu
+				} else _logger.LogInformation("No freebies currently.");
+
+				_logger.LogDebug("Don
