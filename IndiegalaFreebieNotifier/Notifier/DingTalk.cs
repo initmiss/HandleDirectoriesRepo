0fa42a204ae462d52cbbfc7e9046db4084da3c18
initmiss <119107563+initmiss@users.noreply.github.com>
2022-05-14 05:41:21 +0000

    cleanup

--- IndiegalaFreebieNotifier/Notifier/DingTalk.cs
+++ IndiegalaFreebieNotifier/Notifier/DingTalk.cs
@@ -7,3 +7,32 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Logging;
 using IndiegalaFreebieNotifier.Model;
+using IndiegalaFreebieNotifier.Model.PostContent;
+
+namespace IndiegalaFreebieNotifier.Notifier {
+	class DingTalk: INotifiable {
+		private readonly ILogger<DingTalk> _logger;
+
+		#region debug strings
+		private readonly string debugSendMessage = "Send notifications to DingTalk";
+		#endregion
+
+		public DingTalk(ILogger<DingTalk> logger) {
+			_logger = logger;
+		}
+
+		public async Task SendMessage(NotifyConfig config, List<FreeGameRecord> records) {
+			try {
+				_logger.LogDebug(debugSendMessage);
+
+				var url = new StringBuilder().AppendFormat(NotifyFormatStrings.dingTalkUrlFormat, config.DingTalkBotToken).ToString();
+				var content = new DingTalkPostContent();
+
+				foreach (var record in records) {
+					content.Text.Content_ = $"{record.ToDingTalkMessage()}{NotifyFormatStrings.projectLink}";
+					var data = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+					var resp = await new HttpClient().PostAsync(url, data);
+					_logger.LogDebug(await resp.Content.ReadAsStringAsync());
+				}
+
+				_logger.LogDebug($"Done: {debugSendMessage}");
