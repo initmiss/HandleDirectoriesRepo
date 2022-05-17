9d7f97b28562e4cd8990120771657cbae40fa3d5
initmiss <119107563+initmiss@users.noreply.github.com>
2022-05-17 20:34:13 +0000

    Removed unused field

--- IndiegalaFreebieNotifier/Notifier/Discord.cs
+++ IndiegalaFreebieNotifier/Notifier/Discord.cs
@@ -3,4 +3,5 @@ using IndiegalaFreebieNotifier.Models.PostContent;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
-using System.Collectio
+using System.Collections.Generic;
+using System.Ne
