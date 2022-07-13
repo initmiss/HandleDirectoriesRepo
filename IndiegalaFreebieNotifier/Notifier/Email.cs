c50962cc722a25ae736df2e8366e6d9ccdb7009c
initmiss <119107563+initmiss@users.noreply.github.com>
2022-07-13 16:39:33 +0000

    RDS-TCP: Recover correctly from pskb_pull()/pksb_trim() failure in rds_tcp_data_recv

--- IndiegalaFreebieNotifier/Notifier/Email.cs
+++ IndiegalaFreebieNotifier/Notifier/Email.cs
@@ -20,4 +20,4 @@ namespace IndiegalaFreebieNotifier.Notifier {
 			_logger = logger;
 		}
 
-		private MimeMessage CreateMessage(List<FreeGameRecord> pushList, string fromAddress
+		private MimeMessage CreateMessage(List<FreeGameRecord> pushList, string fromAddress, string toAddress)
