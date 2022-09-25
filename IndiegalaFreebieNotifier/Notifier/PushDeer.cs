5f143599e4fa9758b2c668fb0ad85aed177f2d2e
initmiss <119107563+initmiss@users.noreply.github.com>
2022-09-25 19:59:37 +0000

    fixed a compile error

--- IndiegalaFreebieNotifier/Notifier/PushDeer.cs
+++ IndiegalaFreebieNotifier/Notifier/PushDeer.cs
@@ -4,4 +4,5 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using HtmlAgili
+using HtmlAgilityPack;
+using IndiegalaFreebi
