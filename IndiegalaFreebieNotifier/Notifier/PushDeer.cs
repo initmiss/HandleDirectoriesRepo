e193c91c9226ae4ffc8662937e94718808b33fe1
initmiss <119107563+initmiss@users.noreply.github.com>
2022-12-17 19:07:48 +0000

    add:

--- IndiegalaFreebieNotifier/Notifier/PushDeer.cs
+++ IndiegalaFreebieNotifier/Notifier/PushDeer.cs
@@ -40,4 +40,4 @@ namespace IndiegalaFreebieNotifier.Notifier {
 
 				_logger.LogDebug($"Done: {debugSendMessage}");
 			} catch (Exception) {
-				_logger.LogError(
+				_logger.LogError($"Error: {debugSendMessage}")
