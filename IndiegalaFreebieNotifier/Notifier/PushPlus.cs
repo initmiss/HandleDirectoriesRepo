cd8a1edfe590af25055ad83079c2c2caa5c61567
initmiss <119107563+initmiss@users.noreply.github.com>
2022-12-24 18:55:37 +0000

    Removed backup file

--- IndiegalaFreebieNotifier/Notifier/PushPlus.cs
+++ IndiegalaFreebieNotifier/Notifier/PushPlus.cs
@@ -9,3 +9,27 @@ using Microsoft.Extensions.Logging;
 using IndiegalaFreebieNotifier.Model;
 
 namespace IndiegalaFreebieNotifier.Notifier {
+	class PushPlus: INotifiable {
+		private readonly ILogger<PushPlus> _logger;
+
+		#region debug strings
+		private readonly string debugSendMessage = "Send notification to PushPlus";
+		private readonly string debugCreateMessage = "Create notification message";
+		#endregion
+
+		public PushPlus(ILogger<PushPlus> logger) {
+			_logger = logger;
+		}
+
+		private string CreateMessage(List<FreeGameRecord> records) {
+			try {
+				_logger.LogDebug(debugCreateMessage);
+
+				var sb = new StringBuilder();
+
+				records.ForEach(record => sb.AppendFormat(NotifyFormatStrings.pushPlusBodyFormat, record.ToPushPlusMessage()));
+
+				sb.Append(NotifyFormatStrings.projectLinkHTML);
+
+				_logger.LogDebug($"Done: {debugCreateMessage}");
+				return HttpUtility.UrlEncode(sb.ToString());
