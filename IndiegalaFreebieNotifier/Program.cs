c4a5c2e74b25080a83eb0431f632d1eddd57d716
initmiss <119107563+initmiss@users.noreply.github.com>
2023-04-02 15:53:32 +0000

    Update ks.cfg

--- IndiegalaFreebieNotifier/Program.cs
+++ IndiegalaFreebieNotifier/Program.cs
@@ -13,3 +13,33 @@ namespace IndiegalaFreebieNotifier {
 			try {
 				var services = DI.BuildDiAll();
 				logger.Info(" - Start Job -");
+
+				using (services as IDisposable) {
+					// Get telegram bot token, chatID and previous records
+					var jsonOp = services.GetRequiredService<JsonOP>();
+					var config = jsonOp.LoadConfig();
+					services.GetRequiredService<ConfigValidator>().CheckValid(config);
+
+					// Get page source
+					var source = await services.GetRequiredService<Scraper>().GetHtmlSource(config);
+					//var source = System.IO.File.ReadAllText("test.html");
+
+					// Parse page source
+					var parseResult = services.GetRequiredService<Parser>().Parse(source, jsonOp.LoadData());
+
+					//Send notifications
+					await services.GetRequiredService<NotifyOP>().Notify(config, parseResult.PushList);
+
+					// Write new records
+					jsonOp.WriteData(parseResult.Records);
+				}
+
+				logger.Info(" - Job End -\n\n");
+			} catch (Exception ex) {
+				logger.Error(ex.Message);
+			} finally {
+				LogManager.Shutdown();
+			}
+		}
+	}
+}
